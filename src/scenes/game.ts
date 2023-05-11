fcaf83f43302f49c1c205bfab33b554aeea75aaa
Andrii Mykhailenko <andrii@playboxtech.com>
2023-05-11 21:49:16 +0300

    [ADD] added btn-double assets, changed add buttons

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -1,47 +1,89 @@
-import { Scene } from 'phaser';
-
+import { Scene } from "phaser";
+interface IButtonConfig {
+	textureName: string;
+    emptyFrames: number;
+    frameRate: number;
+    background: {
+        width: number;
+        height: number;
+        radius: number;
+    };
+}
+const buttonsConfig: IButtonConfig[] = [
+	{
+		textureName: "btn_hit",
+		emptyFrames: 4,
+		frameRate: 60,
+		background: {
+			width: 150,
+			height: 150,
+			radius: 10
+		}
+	},
+	{
+		textureName: "btn_double",
+		emptyFrames: 4,
+		frameRate: 60,
+		background: {
+			width: 150,
+			height: 150,
+			radius: 10
+		}
+	}
+];
 export class Game extends Scene {
-  constructor() {
-    super({
-      key: 'GameScene'
-    });
-  }
+	constructor() {
+		super({ key: "GameScene" });
+	}
 
-  public create(): void {
-    const texture = this.textures.get('btn_hit');
-    const emptyFrames = 4
-    const frames = texture.getFrameNames();
-    frames.splice(frames.length - emptyFrames, emptyFrames)
-    const animation = this.anims.create({
-      key: 'btnHitAnim',
-      frames: frames.map((frameName, index)=>{
-        return {
-          key: 'btn_hit',
-          frame: index,
-          visible: true
-        }
-      }),
-      frameRate: 30,
-      repeat: 1
-    });
-    this.add.graphics()
-      .fillStyle(0x131b65, 0.6)
-      .fillRoundedRect( 0, 0, 150, 150, 10);
-    if(animation !== false){
+	public create(): void {
+		buttonsConfig.forEach(this.createButton);
+	}
 
-      const sprite = this.add.sprite(0, 0, texture);
-      const lastFrame = animation.frames[animation.frames.length-1];
+	private readonly createButton = (buttonConfig: IButtonConfig, btnIndex: number) => {
+		const { textureName, frameRate, background, emptyFrames } = buttonConfig;
+		const container = this.add.container(background.width * btnIndex + btnIndex * 10, 30);
 
-      sprite.play(animation);
+		const texture = this.textures.get(textureName);
+		const frames = texture.getFrameNames();
+		frames.splice(frames.length - emptyFrames, emptyFrames);
+		const animation = this.anims.create({
+			key: textureName + 'anim',
+			frames: frames.map((frameName, index) => {
+				return {
+					key: textureName,
+					frame: index,
+					visible: true
+				};
+			}),
+			frameRate,
+			repeat: 0
+		});
 
-      sprite.setInteractive();
-      sprite.on('pointerover', ()=>{
-        sprite.play(animation);
-      });
-      sprite.on('pointerout', ()=>{
-        sprite.stopOnFrame(lastFrame);
-      });
-    }
-  }
+		container.add(
+			this.add
+				.graphics()
+				.fillStyle(0x131b65, 0.6)
+				.fillRoundedRect(0, 0, background.width, background.height, background.radius)
+		);
 
+		if (animation !== false) {
+			const sprite = this.add.sprite(background.width*.5, background.height*.5, texture);
+			//sprite.setPosition(-background.width*.5, -background.height*.5);
+			container.add(sprite);
+
+			const firstFrame = animation.frames[0];
+
+			sprite.setInteractive({
+				hitAreaCallback: Phaser.Geom.Circle.Contains,
+				useHandCursor: true
+			});
+			sprite.on("pointerover", () => {
+				sprite.play(animation);
+			});
+			sprite.on("pointerout", () => {
+				sprite.stopOnFrame(firstFrame);
+			});
+		}
+	}
 }
