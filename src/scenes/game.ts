12e16820db2734875cd1ab21c8d834dd3fcf7c98
Andrii Mykhailenko <andrii@playboxtech.com>
2023-05-11 20:39:58 +0300

    [ADD] added spritesheet animation example

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -7,8 +7,41 @@ export class Game extends Scene {
     });
   }
 
-  create(): void {
-    const image = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'phaser_logo');
-    image.setOrigin(0.5);
+  public create(): void {
+    const texture = this.textures.get('btn_hit');
+    const emptyFrames = 4
+    const frames = texture.getFrameNames();
+    frames.splice(frames.length - emptyFrames, emptyFrames)
+    const animation = this.anims.create({
+      key: 'btnHitAnim',
+      frames: frames.map((frameName, index)=>{
+        return {
+          key: 'btn_hit',
+          frame: index,
+          visible: true
+        }
+      }),
+      frameRate: 30,
+      repeat: 1
+    });
+    this.add.graphics()
+      .fillStyle(0x131b65, 0.6)
+      .fillRoundedRect( 0, 0, 150, 150, 10);
+    if(animation !== false){
+
+      const sprite = this.add.sprite(0, 0, texture);
+      const lastFrame = animation.frames[animation.frames.length-1];
+
+      sprite.play(animation);
+
+      sprite.setInteractive();
+      sprite.on('pointerover', ()=>{
+        sprite.play(animation);
+      });
+      sprite.on('pointerout', ()=>{
+        sprite.stopOnFrame(lastFrame);
+      });
+    }
   }
+
 }
